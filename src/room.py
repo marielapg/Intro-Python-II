a4578cd731e574b2d47fa26d52986ca4bf8bc995
marielapg <mpgonzales77@gmail.com>
2020-04-09 16:52:21 -0700

    completed intro to python II day 4

--- src/room.py
+++ src/room.py
@@ -9,8 +9,4 @@ class Room:
         return f"{self.name} {self.description} {self.items}"
     def add_item(self, item):
         self.items.append(item)
-    def remove_item(self, item_name):
-        for index, item in enumerate(self.items):
-            if item.name == item_name:
-                removed_item = self.items.pop(index)
-                return removed_item
+  
